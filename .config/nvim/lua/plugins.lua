5ce1c3039206a5171af222841d230597db0dfb4f
ToruNiina <niina.toru.68u@gmail.com>
2024-01-08 00:52:59 +0900

    feat: add some plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -267,4 +267,51 @@ return {
             })
         end,
     },
+
+    {
+        'jbyuki/venn.nvim',
+        config = function()
+            function _G.Toggle_venn()
+                local venn_enabled = vim.inspect(vim.b.venn_enabled)
+                if venn_enabled == "nil" then
+                    vim.b.venn_enabled = true
+                    vim.cmd.setlocal('ve=all')
+                    -- draw a line on HJKL keystokes
+                    vim.keymap.set('n', 'J', '<C-v>j:VBox<CR>')
+                    vim.keymap.set('n', 'K', '<C-v>k:VBox<CR>')
+                    vim.keymap.set('n', 'L', '<C-v>l:VBox<CR>')
+                    vim.keymap.set('n', 'H', '<C-v>h:VBox<CR>')
+                    -- draw a box by pressing "f" with visual selection
+                    vim.keymap.set('v', 'f', ':VBox<CR>')
+                else
+                    vim.cmd.setlocal('ve=')
+                    vim.cmd.mapclear('<buffer>')
+                    vim.b.venn_enabled = nil
+                end
+            end
+            -- toggle by `:Venn`
+            vim.api.nvim_create_user_command('Venn', ':lua Toggle_venn()', {})
+        end,
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local telescope = require('telescope')
+            telescope.load_extension('fzf')
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
+
+            vim.api.nvim_create_user_command('File', ':Telescope find_files', {})
+            vim.api.nvim_create_user_command('Grep', ':Telescope live_grep', {})
+        end,
+    },
 }
